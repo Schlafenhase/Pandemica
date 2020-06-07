44dfd4132720646553668f3291d5ead3818f5c7b
Jose Acuna <jodaniel1409@gmail.com>
2020-06-07 12:34:57 -0600

    Added summary on home component
    Modified bootstrap theme
    Country and date info
    Cases/Deaths/Active/Recovered count
    Nationals/Foreign count

--- Web/src/app/components/home/home.component.ts
+++ Web/src/app/components/home/home.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // Properties
+  country = 'Costa Rica';
+  lastUpdate = '20-5-2020 10:00 a.m.';
+  confirmed = 42612;
+  active = 35697;
+  dead = 696;
+  recovered = 6219;
+  nationals = 38512;
+  foreign = 4100;
+
   constructor() { }
 
   ngOnInit(): void {
