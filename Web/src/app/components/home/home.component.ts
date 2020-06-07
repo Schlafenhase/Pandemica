238a12526bbcec79b5dd5a33c8528a2221bd9fcc
Jose Acuna <jodaniel1409@gmail.com>
2020-06-07 13:26:04 -0600

    Added current day summary
    Added icons for home

--- Web/src/app/components/home/home.component.ts
+++ Web/src/app/components/home/home.component.ts
@@ -9,13 +9,16 @@ export class HomeComponent implements OnInit {
 
   // Properties
   country = 'Costa Rica';
-  lastUpdate = '20-5-2020 10:00 a.m.';
+  lastUpdate = new Date(2020, 4, 7, 10, 25);
   confirmed = 42612;
   active = 35697;
   dead = 696;
   recovered = 6219;
   nationals = 38512;
   foreign = 4100;
+  newCases = 23;
+  newRecovered = 8;
+  newDeceased = 15;
 
   constructor() { }
 
