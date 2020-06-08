d7d816791d2c2d594efaf77362e37a50862fcf88
Jose Acuna <jodaniel1409@gmail.com>
2020-06-07 20:15:21 -0600

    Added firebase-auth and angular/fire depencencies

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import {AuthComponent} from './components/auth/auth.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
+import { AuthComponent } from './components/auth/auth.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxAuthFirebaseUIModule.forRoot({
+      apiKey: 'AIzaSyCeUVOj8cB5nwhVWa2jxvrc7EgDsGMzGm0',
+      authDomain: 'pandemica-259ec.firebaseapp.com',
+      databaseURL: 'https://pandemica-259ec.firebaseio.com',
+      projectId: 'pandemica-259ec',
+      storageBucket: 'pandemica-259ec.appspot.com',
+      messagingSenderId: '321004965436',
+      appId: '1:321004965436:web:65ed422c723b1096eefd39',
+      measurementId: 'G-3RGXR1MLNC'
+    }),
+    BrowserAnimationsModule,
+    MatPasswordStrengthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
