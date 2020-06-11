8a0926ab785fe31b5bc420437cea7d3ffcb7eb58
Jose Acuna <jodaniel1409@gmail.com>
2020-06-11 13:49:02 -0600

    Implemented AOS animations

--- Web/src/app/components/home/home.component.ts
+++ Web/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {CountryService} from '../../services/country.service';
 
 @Component({
@@ -24,16 +24,41 @@ export class HomeComponent implements OnInit {
   constructor(private countryService: CountryService) { }
 
   ngOnInit(): void {
+    this.getCountryMap();
+  }
+
+  // Updates the country base on the map selection
+  private getCountryMap() {
     this.countryService.country$().subscribe(
       data => {
         this.country = data;
-    });
+      });
   }
 
+  // Gets country data from the API
   private getCountryData() {
     this.countryService.getCountryData()
       .subscribe(
         data => {
         });
+  }
+
+  // Scrolls the user to charts section
+  public onScrollDown(): void {
+    // Scroll certain amounts from current position
+    window.scrollBy({
+      top: 850, // could be negative value
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  backToTop() {
+    window.scroll({
+      top: 0, // could be negative value
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 }
