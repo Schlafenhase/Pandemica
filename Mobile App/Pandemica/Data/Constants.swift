6590b0b31195bf17a1fddd941f33b8c0f9a24185
Alejandro <josealejandroibarra@hotmail.com>
2020-06-23 18:07:38 -0600

    Now with sprinkles.

---
+++ Mobile App/Pandemica/Data/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Pandemica
+//
+//  Created by Alejandro Ibarra on 6/23/20.
+//  Copyright © 2020 Schlafenhase. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let ip = ""
+}
