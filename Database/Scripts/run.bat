610cc17266d806ae6b24097c84f15eeefab1c1b6
Jose Acuna <jodaniel1409@gmail.com>
2020-06-27 12:00:43 -0600

    Added batch file to run data base

--- Database/Scripts/run.bat
+++ Database/Scripts/run.bat
@@ -1,2 +1,6 @@
-for %%G in (*.sql) do sqlcmd /S DESKTOP-VGQH22C /d PandemicaDB -E -i"%%G"
+SET /P _inputserver= Please enter a MS SQL Server:
+
+for %%G in (0000*.sql) do sqlcmd /S DESKTOP-VGQH22C -E -i"%%G"
+
+for %%G in (*.sql) do sqlcmd /S "%_inputname%" /d PandemicaDB -E -i"%%G"
 pause
