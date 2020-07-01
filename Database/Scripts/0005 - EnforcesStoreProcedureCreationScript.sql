a127c1d9cee9e78f9633ed51e9d19f1ee44c5417
Kevin <skryfall@gmail.com>
2020-07-01 10:38:09 -0600

    Updated the scripts

--- Database/Scripts/0005 - EnforcesStoreProcedureCreationScript.sql
+++ Database/Scripts/0005 - EnforcesStoreProcedureCreationScript.sql
@@ -2,8 +2,7 @@ CREATE PROCEDURE GetCountryMeasures
 AS
 SELECT ENFORCES.Country, SANITARY_MEASUREMENTS.Name, ENFORCES.StartDate, ENFORCES.FinalDate, ENFORCES.Id FROM ENFORCES
 INNER JOIN SANITARY_MEASUREMENTS ON SANITARY_MEASUREMENTS.Id = ENFORCES.Measurement;
-
-GetCountryMeasures;
+GO
 
 CREATE PROCEDURE InsertCountryMeasures @Country VARCHAR(30), @Measurement VARCHAR(15), @StartDate DATE, @FinalDate DATE
 AS
@@ -12,8 +11,7 @@ SELECT @SMID = SANITARY_MEASUREMENTS.Id FROM SANITARY_MEASUREMENTS
 WHERE @Measurement = SANITARY_MEASUREMENTS.Name
 INSERT INTO ENFORCES (Country, Measurement, StartDate, FinalDate)
 VALUES (@Country, @SMID, @StartDate, @FinalDate);
-
-InsertCountryMeasures @Country = 'United States', @Measurement = 'Fumigation', @StartDate = '1999-12-12', @FinalDate = '2100-12-12';
+GO
 
 CREATE PROCEDURE UpdateCountryMeasures @Country VARCHAR(30), @Measurement VARCHAR(15), @StartDate DATE, @FinalDate DATE, @Id INT
 AS
@@ -23,5 +21,4 @@ WHERE @Measurement = SANITARY_MEASUREMENTS.Name
 UPDATE ENFORCES
 SET ENFORCES.Country = @Country, ENFORCES.Measurement = @SMID, ENFORCES.StartDate = @StartDate, ENFORCES.FinalDate = @FinalDate
 WHERE ENFORCES.Id = @Id;
-
-UpdateCountryMeasures @Country = 'Angola', @Measurement = 'Fumigation', @StartDate = '2000-12-12', @FinalDate = '2010-12-12', @Id = 3;
+GO
