2a38bb9f2ee036d319597dfd1689844b51daa830
Jose Acuna <jodaniel1409@gmail.com>
2020-07-02 19:15:13 -0600

    Fixed API StoreProcedure restart DB connection

--- API/Source/Server_Connections/Tools.cs
+++ API/Source/Server_Connections/Tools.cs
@@ -32,6 +32,12 @@ namespace API.Source.Server_Connections
                     var value = sqlReader[1];
                     objectList.Add(new JProperty(key, value));
                 }
+                // Adds regions
+                objectList.Add(spCasesByRegion(country));
+                // Adds accumulated
+                objectList.Add(spAccumulatedCasesByCountry(country));
+                // Add measurements
+                
                 Debug.WriteLine(objectList);
                 return objectList;
             }
@@ -42,12 +48,12 @@ namespace API.Source.Server_Connections
             }
         }
 
-        public JObject spCasesByRegion(string country)
+        public JProperty spCasesByRegion(string country)
         {
-            var regions = new JObject();
             var objectList = new JArray();
             try
             {
+                DatabaseDataHolder.restartConnection();
                 SqlCommand cmd = new SqlCommand("spCasesByRegion", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.Add(new SqlParameter("Country", country));
@@ -70,22 +76,21 @@ namespace API.Source.Server_Connections
                     objectList.Add(region);
                 }
                 Debug.WriteLine(objectList);
-                regions.Add(new JProperty("regions", objectList));
-                return regions;
+                return new JProperty("regions", objectList);;
             }
             catch (Exception ex)
             {
                 Debug.WriteLine("An error happened", ex.Message);
-                return regions;
+                return null;
             }
         }
 
-        public JObject spAccumulatedCasesByCountry(string country)
+        public JProperty spAccumulatedCasesByCountry(string country)
         {
-            var accumulatedValues = new JObject();
             var objectList = new JArray();
             try
             {
+                DatabaseDataHolder.restartConnection();
                 SqlCommand cmd = new SqlCommand("spAccumulatedCasesByCountry", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.Add(new SqlParameter("Country", country));
@@ -94,27 +99,26 @@ namespace API.Source.Server_Connections
                 while (sqlReader.Read())
                 {
                     var accumulated = new JObject();
-                    var state = "state";
-                    var stateValue = sqlReader[1];
+                    var date = "date";
+                    var dateValue = sqlReader[0];
                     var quantity = "quantity";
-                    var quantityValue = sqlReader[2];
-                    var regionName = "regionName";
-                    var regionNameValue = sqlReader[0];
+                    var quantityValue = sqlReader[1];
+                    var cases = "cases";
+                    var casesValue = sqlReader[2];
 
-                    accumulated.Add(new JProperty(regionName, regionNameValue));
-                    accumulated.Add(new JProperty(state, stateValue));
+                    accumulated.Add(new JProperty(date, dateValue));
+                    accumulated.Add(new JProperty(cases, casesValue));
                     accumulated.Add(new JProperty(quantity, quantityValue));
 
                     objectList.Add(accumulated);
                 }
                 Debug.WriteLine(objectList);
-                accumulatedValues.Add(new JProperty("regions", objectList));
-                return accumulatedValues;
+                return new JProperty("accumulated", objectList);
             }
             catch (Exception ex)
             {
                 Debug.WriteLine("An error happened", ex.Message);
-                return accumulatedValues;
+                return null;
             }
         }
 
