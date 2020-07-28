5b56d0ae771fe7219e73cb4d5530200600a18e78
Jose Daniel Acuña <38967981+JoDaniel1412@users.noreply.github.com>
2020-07-27 19:41:25 -0600

    Update environment.ts

--- Web/src/environments/environment.ts
+++ Web/src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
   },
   ipAPI: 'localhost',
   portAPI: '44374',
-  serverURL: 'https://localhost:44374/api/'
+  serverURL: 'https://firstwaveapi.azurewebsites.net/api/'
 };
 
 /*
