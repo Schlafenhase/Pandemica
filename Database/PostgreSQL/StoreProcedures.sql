eccc8e9dee618fb3c984f64ca8c1c4b4e6c710fd
Jose Acuna <jodaniel1409@gmail.com>
2020-08-07 02:27:09 -0600

    Added store procedures
    usp_insert_reservation
    usp_insert_bed_equipment
    usp_equipments_from_bed
    usp_update_equipment_from_bed
    usp_delete_bed_equipment

--- Database/PostgreSQL/StoreProcedures.sql
+++ Database/PostgreSQL/StoreProcedures.sql
@@ -52,7 +52,7 @@ begin
 end;
 $$;
 
--- Auxiliary function to get the procedure id from the name
+-- Auxiliary function to get the procedure_id using the name
 create or replace function udf_get_procedure(procedureName varchar(15))
 returns integer
 language plpgsql
@@ -72,5 +72,71 @@ as $$
 begin
     insert into reservation_procedures (procedure_id, reservation_id)
     values (udf_get_procedure(procedureName), reservationId);
+end;
+$$;
+
+-- Auxiliary function to get the equipment_id using the name
+create or replace function udf_get_equipment(equipmentName varchar(15))
+returns integer
+language plpgsql
+as $$
+begin
+    return
+        (select e.id
+        from equipment as e
+        where e.name = equipmentName);
+end;
+$$;
+
+-- Inserts a bed_equipment value
+create or replace procedure usp_insert_bed_equipment(bedNumber integer, equipmentName varchar(15))
+language plpgsql
+as $$
+begin
+    insert into bed_equipment (bed_number, equipment_id)
+    values (bedNumber, udf_get_equipment(equipmentName));
+end;
+$$;
+
+-- Selects all equipments for the given bed
+create or replace function usp_equipments_from_bed(bedNumber integer)
+returns table (Equipment varchar(15))
+language plpgsql
+as $$
+begin
+    return query
+        select e.name
+        from bed_equipment as be
+        inner join equipment e on be.equipment_id = e.id
+        inner join bed b on be.bed_number = b.number
+        where b.number = bed_number;
+end;
+$$;
+
+-- Updates an old equipment for a new one from bed
+create or replace procedure usp_update_equipment_from_bed
+    (
+    bedNumber integer,
+    oldEquipment varchar(15),
+    newEquipment varchar(15)
+    )
+language plpgsql
+as $$
+begin
+    update bed_equipment
+    set equipment_id = udf_get_equipment(newEquipment)
+    where equipment_id = udf_get_equipment(oldEquipment) and
+          bed_number = bedNumber;
+end;
+$$;
+
+-- Deletes a relationship in bed_equipment
+create or replace procedure usp_delete_bed_equipment(bedNumber integer, equipmentId integer)
+language plpgsql
+as $$
+begin
+    delete from bed_equipment
+    where bed_number = bedNumber and
+          equipment_id = equipmentId;
 end;
 $$;
