6daa5741c86f2426394423cb9cd319e8db1d60e4
Kevin <skryfall@gmail.com>
2020-08-06 21:22:37 -0600

    Basic bedequipment structure

---
+++ API/SecondWaveAPIs/StoreProcedures/Source/Entities/EquipmentView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreProcedures.Source.Entities
+{
+    public class EquipmentView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Provider { get; set; }
+        public int Quantity { get; set; }
+
+        public EquipmentView() { }
+
+        public EquipmentView(int eId, string eName, string eProvider, int eQuantity)
+        {
+            Id = eId;
+            Name = eName;
+            Provider = eProvider;
+            Quantity = eQuantity;
+        }
+    }
+}
