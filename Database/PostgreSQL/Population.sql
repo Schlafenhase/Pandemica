44497ffce12e02b27778260c45b21be823632e33
Jose Daniel Acuña <38967981+JoDaniel1412@users.noreply.github.com>
2020-08-08 09:34:57 -0600

    Update Population.sql

--- Database/PostgreSQL/Population.sql
+++ Database/PostgreSQL/Population.sql
@@ -6,3 +6,13 @@ values ('Surgical Lights', 'Silvania', 50),
        ('Monitors', 'Samsung', 1000),
        ('Art. Breathers', 'TEC', 10),
        ('Cardiograph', 'Dianet', 75);
+
+insert into procedure (name, duration)
+values ('Appendectomy,', 5),
+       ('Breast Biopsy', 2),
+       ('Cataract Surg.', 7),
+       ('Caesarean Sect.', 4),
+       ('Hysterectomy', 1),
+       ('Lowback Surgery', 5),
+       ('Mastectomy', 3),
+       ('Tonsillectomy', 2);
